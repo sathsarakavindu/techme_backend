17e896269f20679c0695f74fa982ed6595b948f1
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2025-05-11 13:37:47 +0530

    change pasword api was created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getUsers, postUser, putUser, deleteUser } from '../controllers/userControllers.js';
+import { getUsers, postUser, putUser, deleteUser, changePassword } from '../controllers/userControllers.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/signin/', getUsers);
 
 userRouter.post('/register/', postUser);
+
+userRouter.post('/change-password/', changePassword);
 
 userRouter.put('/update/', putUser);
 
