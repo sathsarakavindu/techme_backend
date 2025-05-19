10eada80cf678848d10484f1cdd20d4b3624c178
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2025-05-19 23:21:00 +0530

    vehicle add with user data

--- controllers/addVehicleController.js
+++ controllers/addVehicleController.js
@@ -4,12 +4,20 @@ import Vehicle from "../model/vehicleModel.js";
 export async function addVehicle(req, res){
 
   try{
+    const name = req.body.name;
+    const email = req.body.email;
+    const contact_no = req.body.contact_no;
+    const nic = req.body.nic;
     const image_url = req.body.image_url;
     const vehicle_no = req.body.vehicle_no;
     const vehicle_type = req.body.type;
     const vehicle_model = req.body.model;
     const vehicle_color = req.body.color;
 
+    console.log(name);
+    console.log(email);
+    console.log(contact_no);
+    console.log(nic);
     console.log(image_url);
     console.log(vehicle_no);
     console.log(vehicle_type);
@@ -17,6 +25,10 @@ export async function addVehicle(req, res){
     console.log(vehicle_color);  
 
 const addVehicel = new Vehicle({
+    name: name,
+    email: email,
+    contact_no: contact_no,
+    nic: nic,
     image_url: image_url,
     vehicle_no: vehicle_no,
     type: vehicle_type,
--- controllers/technicianController.js
+++ controllers/technicianController.js
@@ -2,6 +2,8 @@ import Technician from "../model/technicianRegisterModel.js";
 import bcrypt from "bcrypt";
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import bcryptjs from "bcryptjs";
+import nodemailer from 'nodemailer';
 
 dotenv.config();
 
@@ -99,11 +101,175 @@ export async function signInTechnician(req, res){
 
 }
 
-export async function putTechnician(){
+export async function changePasswordTechnician(req, res){
+    try{
+           const { user_email, current_password, new_password} = req.body;
+            await Technician.findOne({ email: user_email}).then(async (results)=>{
+if(results != null){
+    const isMatch = await bcryptjs.compare(current_password, results.password);
 
+    if(isMatch){
+        const hashedPassword = await bcryptjs.hash(new_password, 8);
+    await Technician.updateOne({password: hashedPassword}).
+        then((results)=>{
+    res.status(200).json({
+        message: "Password changed successfully",
+        result: results
+    });
+        }).catch((error)=>{
+          res.status(400).json({
+          message: "Password can't be changed",
+          error: error
+        });
+      });
+    }
+    else{
+        console.log("Incorrect password");
+        res.status(400).json({
+            message: "Incorrect password",
+            result: results
+        });
+    }
+}
+else{
+    console.log("Incorrect password");
+res.status(400).json({
+    message: "Incorrect password",
+    result: results
+});
+}
+
+            }).catch((err)=>{
+res.status(500).json({message: "change password error", error: err.toString()});
+            });
+
+    }
+    catch(e){
+
+    }
+}
+
+
+export async function sentOTPToTechnician(req, res){
+
+  const {registered_email} = req.body;
+
+  try{
+
+  await Technician.findOne({email: registered_email}).
+  then((user)=>{
+       if(user != null){
+console.log(`User is found: ${user}`);
+      const transporter = nodemailer.createTransport({
+      service: "gmail",  
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+             user: process.env.SERVICE_EMAIL,
+             pass: process.env.SERVICE_EMAIL_PASSWORD,
+            },
+      });
+     const otp = Math.floor(1000 + Math.random() * 9000);
+       const message = {
+                   from: process.env.EMAIL,
+                   to: registered_email,
+                   subject: "Validate OTP",
+                   text: "Your OTP code is " + otp
+          }
+
+       transporter.sendMail(message, (err, info)=>{
+
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log(info);
+
+        Technician.findOneAndUpdate({email: registered_email}, {otp: otp}, {new: true}).
+        then((result)=>{
+          res.status(200).json({
+            message: "OTP sent successfully",
+            result: result
+          });
+        }).
+        catch((error)=>{
+          res.status(400).json({
+            message: "OTP can't be sent",
+            error: error
+          });
+        });
+
+      }
+    });
+
+
+       }
+       else{
+        res.status(400).json({message: "Invalid Email"});
+       }
+
+  });
+
+    }
+  catch(e){
+    console.log(`OTP sending error is ${e.toString()}`);
+      res.status(500).json({
+        message: "OTP can't be sent!",
+        error: e.toString()
+      });
+  }
 
 }
 
-export async function deleteTechnician(){
+export async function checkOTPValidationForTechnician(req, res){
+
+    try{
+         const {sent_otp} = req.body;
+        //  const hashedPassword = await bcryptjs.hash(new_password, 8);
+
+         await Technician.findOne({otp: sent_otp}).
+       then((results)=>{
+           if(results != null){
+               res.status(200).json({
+               message: "OTP is correct!"
+               });
+           }
+           else{
+              res.status(400).json({
+               message: "OTP is incorrect!"
+               });
+           }
+       })
+
+    }
+    catch(e){
+
+    }
+}
+
+export async function forgotPasswordUpdateForTechnician(req, res){
+
+     try{
+            const {new_password, otp} = req.body;
+            const hashedPassword = await bcryptjs.hash(new_password, 8);
+           await Technician.findOneAndUpdate({otp: otp}, {password: hashedPassword}, {new: true}).
+            then((results)=>{
+              
+                res.status(200).json({
+                    message: "Technician Password updated successfully",
+                    result: results
+                });
+            }).
+            catch((error)=>{
+                res.status(400).json({
+                    message: "Password can't be updated",
+                    error: error
+                });
+            });
+     }
+     catch(e){
+
+     }
 
 }
