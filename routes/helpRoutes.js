3cc1aab40f769f9c82921600228639b37663a675
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2025-06-01 13:03:29 +0530

    cancel help API created

--- routes/helpRoutes.js
+++ routes/helpRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { makeHelp } from "../controllers/helpController.js";
+import { cancellMadeHelp, makeHelp } from "../controllers/helpController.js";
 
 const helpRouter = express.Router();
 
 helpRouter.post('/make-help/', makeHelp);
-
+helpRouter.post('/cancel-help/', cancellMadeHelp);
 
 export default helpRouter;
